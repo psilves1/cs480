ad07aec7fd30a8f7431a0723ddc7663b88df7a5a
psilves1 <psilvestri9@gmail.com>
2022-04-10 14:12:08 -0400

    Finished part 1

--- Project/CovertChannel-FlushReload/receiver.c
+++ Project/CovertChannel-FlushReload/receiver.c
@@ -3,7 +3,6 @@
 // Cache miss latency threshold
 int CACHE_MISS_LATENCY = 100;
 
-
 bool detect_bit(struct config *config)
 {
 	int misses = 0;
@@ -11,14 +10,17 @@ bool detect_bit(struct config *config)
 
 	// Sync with sender
 	CYCLES start_t = cc_sync();
-	while ((get_time() - start_t) < config->interval) {
-		
-		CYCLES access_time = measure_one_block_access_time(config->addr); 
+	while ((get_time() - start_t) < config->interval)
+	{
 
-		
-		if (access_time > CACHE_MISS_LATENCY) {
+		CYCLES access_time = measure_one_block_access_time(config->addr);
+
+		if (access_time > CACHE_MISS_LATENCY)
+		{
 			misses++;
-		} else {
+		}
+		else
+		{
 			hits++;
 		}
 	}
@@ -34,33 +36,42 @@ int main(int argc, char **argv)
 	char msg_ch[MAX_BUFFER_LEN + 1];
 
 	uint32_t bitSequence = 0;
-	uint32_t sequenceMask = ((uint32_t) 1<<6) - 1; //1 000 000 - 1 ----->>>  11 11 11 & (XXXXXXX 10 10 11) === 10 10 11
+	uint32_t sequenceMask = ((uint32_t)1 << 6) - 1; // 1 000 000 - 1 ----->>>  11 11 11 & (XXXXXXX 10 10 11) === 10 10 11
 	uint32_t expSequence = 0b101011;
-	
+
 	printf("Listening...\n");
-	fflush(stdout); //SPY FLUSHES STDOUT
-	while (1) {
+	fflush(stdout);
+	while (1)
+	{
 		bool bitReceived = detect_bit(&config);
 
-		//1  0   101011		0101011 0 -> 0101011 1
-		// Detect the sequence '101011' that indicates sender is sending a message	
-		bitSequence = ((uint32_t) bitSequence<<1) | bitReceived;
-		if ((bitSequence & sequenceMask) == expSequence) {  
+		// 1  0   101011		0101011 0 -> 0101011 1
+		//  Detect the sequence '101011' that indicates sender is sending a message
+		bitSequence = ((uint32_t)bitSequence << 1) | bitReceived;
+		if ((bitSequence & sequenceMask) == expSequence)
+		{
 			int binary_msg_len = 0;
 			int strike_zeros = 0;
-			for (int i = 0; i < MAX_BUFFER_LEN; i++) {
+			
+			for (int i = 0; i < MAX_BUFFER_LEN; i++)
+			{
 				binary_msg_len++;
 
-				if (detect_bit(&config)) {
+				if (detect_bit(&config))
+				{
 					msg_ch[i] = '1';
 					strike_zeros = 0;
-				} else {
+				}
+				else
+				{
 					msg_ch[i] = '0';
-					if (++strike_zeros >= 8 && i % 8 == 0) {// checks for	0000 0000. AKA end of the message
+					if (++strike_zeros >= 8 && i % 8 == 0)
+					{ // checks for	0000 0000. AKA end of the message
 						break;
 					}
 				}
 			}
+			
 
 			/*
 			msg_ch[binary_msg_l	en - 8] = '\0';  //removes the end 0000 0000 byte and adds '/0' to mark end of the string
@@ -69,39 +80,41 @@ int main(int argc, char **argv)
 			int ascii_msg_len = binary_msg_len / 8; //converts bin len to ascii len
 			char msg[ascii_msg_len]; //creates message buffer
 			printf("> %s\n", conv_char(msg_ch, ascii_msg_len, msg)); //converts from bin to ascii and prints
-	
+
 			// Terminate loop if received "exit" message
 			if (strcmp(msg, "exit") == 0) {
 				break;
 			}
-			*/	
+			*/
 
+			FILE *ptr = fopen("/home/psilves1/Desktop/cs480/Project/CovertChannel-FlushReload/senderFile.txt", "r");
 
+			char *line;
+			int len;
 
-			FILE *ptr = fopen("/home/psilves1/Desktop/cs480/Project/CovertChannel-FlushReload/senderFile2.txt", "rb");
+			char buf[4096];
+			fgets(buf, 4096, ptr);
+			// Convert that message to binary
+			char *msg = string_to_binary(buf);
+			int matches = 0;
 
-			char *msg = string_to_binary(ptr);
-
-
-			float matches = 0;
-
-			for(int i = 0; i < binary_msg_len; i++){
-				if(msg_ch[i] == msg[i]){
+			for (int i = 0; i < binary_msg_len; i++)
+			{
+				if (msg_ch[i] == msg[i])
+				{
 					matches++;
 				}
 			}
 
-			float succRate = matches / binary_msg_len;
+			float succRate = ((float)matches / binary_msg_len);
 
 			printf("Error Rate: %f" , (1 - succRate));
+			//printf("hits are : %d", matches);
 
 			break;
-
 		}
 	}
 
 	printf("\nReceiver finished\n");
 	return 0;
 }
-
-
--- Project/CovertChannel-FlushReload/sender.c
+++ Project/CovertChannel-FlushReload/sender.c
@@ -35,24 +35,33 @@ int main(int argc, char **argv) {
 		char text_buf[128];////
 		fgets(text_buf, sizeof(text_buf), stdin);
 
-		uint32_t start = get_time();
 
 		// Indicate termination if input message is "exit"
 		if (strcmp(text_buf, "exit\n") == 0) {
 			sending = 0;
 		}
 
-		FILE *ptr = fopen("/home/psilves1/Desktop/cs480/Project/CovertChannel-FlushReload/senderFile.txt", "rb");
+		FILE *ptr = fopen("/home/psilves1/Desktop/cs480/Project/CovertChannel-FlushReload/senderFile.txt", "r");
 
+		char *line;
+		int len;
+
+
+		char buf[4096];
+		fgets(buf, sizeof(buf), ptr);
 		// Convert that message to binary
-		char *msg = string_to_binary(ptr);
+		char *msg = string_to_binary(buf);
 
+		uint32_t start = get_time();
+
+		
 		// Send a '10101011' bit sequence tell the receiver
 		// a message is going to be sent
 		for (int i = 0; i < 8; i++) {
 			send_bit(sequence[i], &config); 	//sends the expected sequence
 		}
 
+		
 		
 		size_t msg_len = strlen(msg);
 		for (int ind = 0; ind < msg_len; ind++) {
@@ -61,11 +70,13 @@ int main(int argc, char **argv) {
 			} else {
 				send_bit(true, &config);   //sends bit
 			}
+			if(ind%10 == 0)
+				send_bit(true, &config);
 		}
 
 		uint32_t end = get_time();
 
-		float timeSecs = (end-start)/(1.305 * 1000000000);
+		float timeSecs = (end-start)/(1.8 * 1000000000);
 
 		printf("Time in seconds = %f", timeSecs);
 
